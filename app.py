3107e50c50990e5f6ba1345090b3d4508124f0f5
alexanderkrasnov <45015498+alexanderkrasnov@users.noreply.github.com>
2026-02-27 21:42:53 +0400

    Update app.py

--- app.py
+++ app.py
@@ -274,4 +274,5 @@ def _default_tokens() -> dict:
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    port = int(os.getenv("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
